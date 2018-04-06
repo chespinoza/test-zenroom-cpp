2a75cb88b289c86d73fc8d8573a01ccbacde312b
Christian Espinoza <chespinoza@gmail.com>
2018-04-06 18:47:02 +0100

    add main file

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../lib/zenroom/src/zenroom.h"
+#ifdef __cplusplus
+}
+#endif
+
+static char *Readfile(const char *filename)
+{
+    std::ifstream fin(filename);
+    std::filebuf *pbuff = fin.rdbuf();
+    std::size_t size = pbuff->pubseekoff(0, fin.end, fin.in);
+    pbuff->pubseekpos(0, fin.in);
+    char *buffer = new char[size];
+
+    pbuff->sgetn(buffer, size);
+    fin.close();
+    return buffer;
+}
+
+int main(int argc, char *argv[])
+{
+
+    char *script = Readfile("scripts/test.lua");
+    char *data = (char *)"{\"val\":1}";
+    char *keys = Readfile("keys/device_keys.json");
+
+    return zenroom_exec(script, NULL, keys, data, 1);
+}
